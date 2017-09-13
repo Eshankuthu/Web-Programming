b47f7aa8fb94902208b0ca32a88ac10593adcc0f
eshankuthu <kuthu.eshan@gmail.com>
2017-09-13 15:09:52 -0500

    Adding more File

---
+++ Javascript/JS-Module-Pattern/app.js
@@ -0,0 +1,24 @@
+console.log("hi module pattern")
+
+const appModule =(function(){
+      let counter =0;
+      return{
+          setCounter:function(newValue){
+            counter = newValue;
+          },
+
+          incrementCounter : function(){
+              return ++counter;
+          },
+
+          getCounter : function(){
+            return counter;
+          }
+      }
+}))();
+
+// Use our module
+
+console.log(appModule.getCounter());
+let newValue = appModule.incrementCounter();
+console.log(newValue);
